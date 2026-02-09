77c805008ea7017a85a5a5f8e5ed6b121d63735d
Teddy_Brooks <lee.brooks.caroca@gmail.com>
2026-02-09 12:05:15 -0300

    ...

---
+++ Proyecto_integrador/Etapa_II.py
@@ -0,0 +1,42 @@
+# =====================================================
+#   PYTHON PARA ANÁLISIS DE DATOS
+#   PROYECTO INTEGRADOR - ETAPA 2
+#   ANÁLISIS DE DELITOS EN LA CIUDAD DE BUENOS AIRES
+# =====================================================
+
+# =====================================================
+#   CONSIGNA GENERAL
+# =====================================================
+# En esta segunda parte del integrador vamos a descargar un dataset llamado
+# delitos2019.csv, que se encuentra para descargar junto a este enunciado.
+# Es un archivo que contiene todos los delitos reportados en la Ciudad de
+# Buenos Aires durante el 2019.
+#
+# (El dataset es provisto por la página oficial del gobierno de la Ciudad de
+# Buenos Aires. Está para descargar directamente de la clase, para que todos
+# trabajemos con el mismo y no perdamos tiempo buscándolo).
+
+# =====================================================
+#   ANÁLISIS REQUERIDO
+# =====================================================
+# Una vez que cargamos el archivo como Dataframe, vamos a analizar varias
+# cuestiones:
+#
+# 1. Lo primero es averiguar qué tipos de datos e información tenemos en el
+#    Dataframe con info()
+#
+#    Si fuera necesario, podemos ir intercalando entre celdas de código, celdas
+#    de texto para darle énfasis al análisis con Markdown e imágenes.
+#
+# 2. La cantidad de delitos totales denunciados durante el 2019.
+#
+# 3. El top 5 de barrios con más delitos.
+#
+# 4. El top 5 de barrios con menos delitos.
+#
+# 5. El horario (franja horaria) ordenado de forma descendente desde el peor
+#    horario al menos problemático.
+
+# =====================================================
+#    FIN ETAPA 2 - PROYECTO INTEGRADOR
+# =====================================================
